90960a2a783216a73a8e1290136374cc33ed2b03
cyber-cityz <info@reggywilliams.com>
2025-02-25 17:55:07 +0000

    Initial commit

---
+++ App1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        NewMethod();
+
+        static void NewMethod()
+        {
+            Console.WriteLine("##################################");
+
+            int a = 1;
+
+            Console.WriteLine(a);
+
+            int b = a++;
+
+            Console.WriteLine("##################################");
+
+            Console.WriteLine(a);
+
+            Console.WriteLine(b);
+
+            Console.WriteLine("##################################");
+
+        }
+    }
+}
